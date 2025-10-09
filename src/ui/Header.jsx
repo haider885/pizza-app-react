6e568a8f18d5e4db39af0c2c77c20a3c272e6871
Haider Akbar <haider.akbarr@gmail.com>
2025-10-10 01:30:09 +0500

    Add App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast Pizza</Link>
+      <p>Haider</p>
+    </header>
+  );
+}
+
+export default Header;
