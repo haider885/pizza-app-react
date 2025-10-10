b5d60b4ed3067e26a8171d6da09f5b9d1ef40686
Haider Akbar <haider.akbarr@gmail.com>
2025-10-11 00:24:19 +0500

    Add Styling

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
-  plugins: ["prettier-plugin-tailwindcss"],
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
   singleQuote: true,
 };
